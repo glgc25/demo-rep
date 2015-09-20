4855758b41f9c745ffe1c84ed8239b40745bfcae
Danil Kazakov <glgc25@gmail.com>
2015-09-20 15:23:28 +0500

    Добавил index.php в demo-rep

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php 
+	echo "<h1>Какой-то заголовок</h1>";
+	echo "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio veniam sint veritatis iusto obcaecati ex, delectus et accusamus natus ab, placeat odio dolor laudantium accusantium fugit iure nemo praesentium voluptas!</p>";
+ ?>
