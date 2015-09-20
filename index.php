13ab6e54025aec378cb53479750efec3d2914bda
Danil Kazakov <glgc25@gmail.com>
2015-09-20 15:49:21 +0500

    Обновил файлы * README.MD * index.php * style.css

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<head>
+	<title>Заголовок страницы</title>
+</head>
+<body>
+
 <?php 
 	echo "<h1>Какой-то заголовок</h1>";
 	echo "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio veniam sint veritatis iusto obcaecati ex, delectus et accusamus natus ab, placeat odio dolor laudantium accusantium fugit iure nemo praesentium voluptas!</p>";
